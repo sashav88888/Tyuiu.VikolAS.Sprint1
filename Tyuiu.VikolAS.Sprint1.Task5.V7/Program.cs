1db052457db3c3546532e707337a9f1f3b790111
alex <sashavikol7@gmail.com>
2025-10-13 20:06:32 +0500

    Update Program.cs

--- Tyuiu.VikolAS.Sprint1.Task5.V7/Program.cs
+++ Tyuiu.VikolAS.Sprint1.Task5.V7/Program.cs
@@ -1,35 +1,49 @@
 ﻿using System;
-using Tyuiu.VikolAS.Sprint1.Task5.V7.Lib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Tyuiu.VikolAS.Sprint1.Task5.V7
+using Tyuiu.VikolAS.Sprint1.Task7.V5.Lib;
+namespace Tyuiu.VikolAS.Sprint1.Task7.V5
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+
             Console.Title = "Спринт #1 | Выполнил: Викол А.С. | ИСПб-25-1";
             Console.WriteLine("*****************************************");
             Console.WriteLine("* Спринт #1                             *");
             Console.WriteLine("* Тема: Базовые навыки работы с C#      *");
-            Console.WriteLine("* Задание #5                            *");
-            Console.WriteLine("* Вариант #7                            *");
+            Console.WriteLine("* Задание #7                            *");
+            Console.WriteLine("* Вариант #5                            *");
             Console.WriteLine("* Выполнил: Викол А.С. | ИСПб-25-1      *");
             Console.WriteLine("*****************************************");
-            Console.WriteLine("* УСЛОВИЕ                               *");
-            Console.WriteLine("* Определить h – полное количество часов прошедших от начала суток до того момента*");
-            Console.WriteLine("* когда часовая стрелка повернулась на f градусов (0<f<360, f – вещественное число). *");
+            Console.WriteLine("* УСЛОВИЕ:                              *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по исходным значениям данных, вводимых пользователем.*");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой. *");
             Console.WriteLine("*****************************************");
 
 
 
-            Console.Write("Введите угол: ");
-            double f = Convert.ToDouble(Console.ReadLine());
 
-            DataService ds = new DataService();
-            var res = ds.AngleToHoursMinutes(f);
+            Console.Write("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine($"{res.Hours} часов {res.Minutes} минут");
+            double result = ds.Calculate(x);
+
+            Console.WriteLine("*****************************************");
+            Console.WriteLine($"Результат: z = {result}");
+            Console.WriteLine("*****************************************");
+
             Console.ReadKey();
+
+
+
         }
     }
 }
+
